c317c0d01ec5020169d851727c5f8ed9b2d251c4
Oleksandr Z <cyvubino@protonmail.com>
2022-01-19 16:07:21 +0200

    added typescript and useful packages

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,14 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-etherscan";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const {PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+ export default {
   networks: {
     polygon: {
       url: `https://rpc-mainnet.matic.quiknode.pro`,
