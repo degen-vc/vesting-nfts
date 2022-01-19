f8e54684f5f87b50a1459d11e76e1e9abff5dd57
Oleksandr Z <cyvubino@protonmail.com>
2022-01-19 16:55:13 +0200

    updated TypeScript settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,8 @@
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
 import * as dotenv from "dotenv";
 dotenv.config();
 
@@ -8,7 +11,7 @@ const {PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
- export default {
+export default {
   networks: {
     polygon: {
       url: `https://rpc-mainnet.matic.quiknode.pro`,
@@ -19,7 +22,13 @@ const {PRIVATE_KEY, ETHERSCAN_API_KEY} = process.env;
       accounts: [PRIVATE_KEY],
     }
   },
-  solidity: "0.8.10",
+  solidity: "0.8.9",
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1
+    }
+  },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY
   }
